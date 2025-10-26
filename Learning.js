52ed2d061143f65113a5c044bbfd70ce44b31a79
Shrutipurusothaman <shrutikishore04@gmail.com>
2025-10-26 14:39:52 +0530

    New js learning day 1

---
+++ Learning.js
@@ -0,0 +1,30 @@
+/*Game developer
+1.players name
+2.players score
+3.player is online or not
+4.players age
+*/ 
+
+let players_name = typeof "shruti" //without bracket
+let players_score = typeof(70)  //with brackets (it can be used either of the way, brackets are used for a clarity of the content visbility)
+const player_online=typeof(true)
+const player_age= typeof null  // NOTE: If I have defined something null its type will display as object bcoz of js quirk.
+console.log(players_name)
+console.log(players_score)
+console.log(player_online)
+console.log(player_age)
+
+/*
+
+1.typeof operator is used to check what is the datatype of the value or variable
+syntax:
+   typeof value_or_variable
+
+2.Uses:
+  >>> Helps you debug your code — you know exactly what type of value you’re working with.
+
+  >>> Helps avoid errors in calculations or operations (like adding a number and text accidentally).
+
+  >>> Helps in conditional checks, e.g., “if this variable is a string, do X; if it’s a number, do Y.”
+*/
+
